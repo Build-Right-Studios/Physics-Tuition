dc453f5c3736e90c60b2fa8154038f1a1298f467
Aryesh02 <aryeshsrivastava@gmail.com>
2026-03-17 02:40:49 +0530

    Backend changes 1

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://questiondesk-frontend.vercel.app",
+  ],
   credentials: true,
 }));
 
